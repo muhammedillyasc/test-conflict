ce535d0a8a96177cc972959567c1ecbd4c47f0eb
Illyas <illyas.deulect@gmail.com>
2022-03-09 09:54:08 +0530

    base commit

---
+++ a.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const a = () => {
+  return (
+    <View>
+      <Text>a</Text>
+    </View>
+  );
+};
+
+export default a;
+
+const styles = StyleSheet.create({});
